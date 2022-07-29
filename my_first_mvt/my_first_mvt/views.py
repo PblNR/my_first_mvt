4c6a04d891442d1aff98e51b90c00b3b2d86ceb9
PblNR <pablo.n.rabal@gmail.com>
2022-07-29 00:03:10 -0300

    commit final

--- my_first_mvt/my_first_mvt/views.py
+++ my_first_mvt/my_first_mvt/views.py
@@ -2,22 +2,6 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.shortcuts import render
 
-def saludo(request):
-    return HttpResponse('Hola mundo')
-
-def dia_de_hoy(request):
-    hoy = datetime.today().date()
-    return HttpResponse(f'Hoy es {hoy}')
-
-def saludo_nombre(request, nombre):
-        return HttpResponse(f'Hola {nombre}')
-
-def nacimiento(request, edad):
-    anio_actual = datetime.today().year
-    anio = anio_actual - edad
-    #anio = edad
-    return HttpResponse(f'Naciste en: {anio}')
-
 def plantilla(request):
     today = datetime.today().date
     context = {

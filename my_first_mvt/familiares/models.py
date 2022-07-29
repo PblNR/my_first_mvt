238ceefffa9f0d6d2dd5fbb28d53b3048d89642b
PblNR <pablo.n.rabal@gmail.com>
2022-07-28 23:18:56 -0300

    modelo relatives

--- my_first_mvt/familiares/models.py
+++ my_first_mvt/familiares/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class relatives(models.Model):
+    first_name = models.CharField(max_length = 40)
+    last_name = models.CharField(max_length = 40)
+    age = models.IntegerField()
+    birthday = models.DateField(max_length=10)
+ 

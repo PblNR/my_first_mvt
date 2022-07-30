3aa38a0da839930d000c6dab389eacce7e6247a3
PblNR <pablo.n.rabal@gmail.com>
2022-07-30 14:36:28 -0300

    Prubea3

--- my_first_mvt/familiares/models.py
+++ my_first_mvt/familiares/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Relatives(models.Model):
     name = models.CharField(max_length = 80)
     age = models.IntegerField()
-    #birthday = models.DateField(default = 10/10/1950)
+    birth_date = models.DateField(default = '1950-10-10')
  
  #No conseguí hacer que funcione la edad
--- my_first_mvt/familiares/views.py
+++ my_first_mvt/familiares/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from familiares.models import Relatives
 
 def create_relative(request):
-    relative = Relatives.objects.create(name = 'Jose', age = 20)
+    relative = Relatives.objects.create(name = 'Jose', age = 20, birth_date = '1900-1-1')
     context = {
         'new_relative': relative
     }
